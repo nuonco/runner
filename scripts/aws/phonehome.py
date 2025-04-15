90ab721235b9a01b99b09c1b3fac25c2f106e446
sam boyer <sam@nuon.co>
2025-04-15 14:29:48 -0400

    feat: Add init and phonehome scripts

---
+++ scripts/aws/phonehome.py
@@ -0,0 +1,35 @@
+import json
+
+import urllib3
+import cfnresponse
+
+http = urllib3.PoolManager()
+
+
+def lambda_handler(event, context):
+    props = event["ResourceProperties"]
+    
+    # Start with all fields from the event
+    data = event.copy()
+    # Remove ResourceProperties since we'll flatten those separately
+    props = data.pop("ResourceProperties", None)
+    
+    encoded_data = json.dumps(props).encode("utf-8")
+    url = props["url"]
+
+    try:
+        response = http.request(
+            "POST",
+            url,
+            body=encoded_data,
+            headers={"Content-Type": "application/json"},
+        )
+        print("Response: ", response.data)
+        cfnresponse.send(event, context, cfnresponse.SUCCESS, {})
+    except Exception as e:
+        print("Error: ", str(e))
+        # It's OK if notifying Nuon fails on deletion
+        if event["RequestType"] in ["Create", "Update"]:
+            cfnresponse.send(event, context, cfnresponse.FAILED, {"Error": str(e)})
+        else:
+            cfnresponse.send(event, context, cfnresponse.SUCCESS, {})
